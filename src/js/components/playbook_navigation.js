29fdbda1a3065f599db5bce680c3902acd380eab
Bruno Rodrigues <brunowebr@gmail.com>
2019-10-11 18:12:13 -0300

    Add active items behavior for section navigations

--- src/js/components/playbook_navigation.js
+++ src/js/components/playbook_navigation.js
@@ -1,6 +1,7 @@
 function playbookNavigation() {
 	var $navigation_wrapper = $('.js-playbook-navigation-wrapper');
 	var $navigation = $('.js-playbook-navigation');
+	var $sections = $('.article section');
 
 	$(window).on('scroll', function(){
 		if($(window).scrollTop() >= $navigation_wrapper.offset().top) {
@@ -8,5 +9,18 @@ function playbookNavigation() {
 		} else if($(window).scrollTop() < $navigation_wrapper.offset().top) {
 			$navigation.removeClass('is-fixed');
 		}
-	})
+
+		$sections.each(function(){
+			var position = $(this).offset().top;
+			var position_bottom = position + $(this).outerHeight();
+
+			if($(window).scrollTop() == position || $(window).scrollTop() <= position_bottom) {
+				var elID = $(this).attr('id');
+
+				$('.nav-link:not([href="#'+elID+'"])').removeClass('active');
+				$('.nav-link[href="#'+elID+'"]').addClass('active');
+				return false;
+			}
+		});
+	});
 }
